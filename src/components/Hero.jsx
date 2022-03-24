2eae3c821bf477f83cb60f8368fb24317d08d860
SamucaBraga <samuelbgf@hotmail.com>
2022-03-24 20:24:08 -0300

    textShadow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,15 +7,28 @@ export default function Hero() {
         <div className="lg:grid lg:grid-cols-2 lg:gap-8">
           <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
             <div className="lg:py-24">
-              <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+              <h1
+                style={{
+                  textShadow:
+                    "0 0 80px rgb(192 219 255 / 48%), 0 0 32px rgb(65 120 255 / 24%)",
+                }}
+                className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl"
+              >
                 <span className="block">Blockchain Security </span>
-                <span className="block text-primary">& Smart Contract Audits</span>
+                <span className="block text-primary">
+                  & Smart Contract Audits
+                </span>
               </h1>
-              <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+              {/* <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat.
-              </p>
+                Lorem cupidatat commodo.
+              </p> */}
+              <button
+                type="button"
+                className="inline-flex items-center px-5 py-2 border border-transparent mt-3 text-base sm:mt-7 sm:text-xl lg:text-lg xl:text-xl leading-4 font-medium rounded text-white bg-indigo-400 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Explore
+              </button>
             </div>
           </div>
           <div className="mt-12 -mb-16 sm:-mb-48 lg:relative">

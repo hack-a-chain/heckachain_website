2b2f69efc3da7ccc437c1013803f1961002854be
Samuel Braga <samuel@digitalhorizon.sv>
2022-04-01 09:54:11 -0300

    header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header({ move }) {
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Popover.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-grayHack focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Popover.Button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-grayHack focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (

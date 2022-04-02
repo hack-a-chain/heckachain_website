2410b9846aeea94285be3dd105c8e86c46249a76
Samuel Braga <samuel@digitalhorizon.sv>
2022-04-02 14:49:05 -0300

    fix css

--- pages/Development.jsx
+++ pages/Development.jsx
@@ -1,7 +1,14 @@
+import Form from "../src/components/Form";
 import HeroDev from "../src/components/HeroDev";
 
 export default function Development() {
   return (
-    <HeroDev />
+    <div>
+      <HeroDev />
+      <div className="h-screen bg-primary">
+
+      </div>
+      <Form />
+    </div>
   );
 }
--- src/components/HeroDev.jsx
+++ src/components/HeroDev.jsx
@@ -4,7 +4,7 @@ import BlockChainAnimate from "./BlockChainAnimate";
 
 export default function HeroDev() {
   return (
-    <div className="relative bg-black">
+    <div className="relative bg-black pt-20">
       <div className="absolute inset-x-0 bottom-0">
         <svg
           viewBox="0 0 224 12"
@@ -15,7 +15,7 @@ export default function HeroDev() {
           <path d="M0,0 C48.8902582,6.27314026 86.2235915,9.40971039 112,9.40971039 C137.776408,9.40971039 175.109742,6.27314026 224,0 L224,12.0441132 L0,12.0441132 L0,0 Z" />
         </svg>
       </div>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-24">
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
         <div className="relative max-w-2xl sm:mx-auto sm:max-w-xl md:max-w-2xl sm:text-center">
           <h2 className="mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none lg:text-5xl">
             Build your Web 3.0 project

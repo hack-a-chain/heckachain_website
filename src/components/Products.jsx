cb479feb76cad8e2466e4f691b720060693530cb
Samuel Braga <samuel@digitalhorizon.sv>
2022-04-12 14:01:08 -0300

    Products css

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -27,9 +27,9 @@ export default function Products() {
   return (
     <div className="bg-black overflow-hidden relative">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-20 lg:px-8 md:py-16">
-        <div className="lg:grid lg:grid-cols-2 lg:gap-24 lg:items-center">
-          <div data-aos="fade-up  z-50">
-            <h2 className="text-3xl font-extrabold text-white sm:text-4xl xl:text-5xl">
+        <div className="lg:grid lg:grid-cols-2 lg:gap-16 lg:items-center">
+          <div data-aos="fade-up ">
+            <h2 className="text-3xl z-50 font-extrabold text-white sm:text-4xl xl:text-5xl">
               Work experience with all kinds of projects in the crypto world.
             </h2>
             <div className="mt-6">
@@ -44,27 +44,49 @@ export default function Products() {
             </div>
           </div>
 
-          <div className="mt-8 grid grid-cols-2 gap-1 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
-            <img
-              src="/graphics/grafismo-03.png"
-              alt="grafismo"
-              className="absolute opacity-40 sm:-top-5 right-5"
-            />
-            {logos.map((logo, index) => (
-              <div
-                data-aos="zoom-in-left"
-                className={`${index === 4 ? 'col-span-2' : 'col-span-1'} py-4 z-40 h-28 w-28 bg-white backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-xl ml-2 rounded-full flex justify-center items-center p-5 px-1 m-2 cursor-pointer ${
-                  index % 2 !== 0 ? "animate-bounce" : "animate-bounceFirst"
-                }`}
-                key={index}
-              >
-                <img
-                  className="max-h-20 transition duration-500 ease-in-out"
-                  src={logo.img}
-                  alt={logo.alt}
-                />
-              </div>
-            ))}
+          <img
+            src="/graphics/grafismo-03.png"
+            alt="grafismo"
+            className="absolute opacity-40 sm:-top-5 right-5"
+          />
+
+          <div className="flex items-center justify-evenly">
+            <div className="mt-8 lg:mt-0 flex flex-col">
+              {logos.splice(0, 3).map((logo, index) => (
+                <div
+                  data-aos="zoom-in-left"
+                  className={`py-4 z-40 h-28 w-28 bg-white backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-xl ml-2 rounded-full flex justify-center items-center p-5 px-1 m-2 cursor-pointer ${
+                    // index % 3 !== 0 ? "animate-bounce" : "animate-bounceFirst"
+                    "animate-bounce"
+                  }`}
+                  key={index}
+                >
+                  <img
+                    className="max-h-20 transition duration-500 ease-in-out"
+                    src={logo.img}
+                    alt={logo.alt}
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="mt-8 lg:mt-0 flex flex-col">
+              {logos.map((logo, index) => (
+                <div
+                  data-aos="zoom-in-left"
+                  className={`py-4 z-40 h-28 w-28 bg-white backdrop-filter backdrop-blur-sm bg-opacity-10 shadow-xl ml-2 rounded-full flex justify-center items-center p-5 px-1 m-2 cursor-pointer ${
+                    // index % 3 !== 0 ? "animate-bounce" : "animate-bounceFirst"
+                    "animate-bounceFirst"
+                  }`}
+                  key={index}
+                >
+                  <img
+                    className="max-h-20 transition duration-500 ease-in-out"
+                    src={logo.img}
+                    alt={logo.alt}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>

2bfd3c415b3433a9690a9543f71845b493d1019c
SamucaBraga <samuelbgf@hotmail.com>
2022-04-19 18:01:58 -0300

    config SEO

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from "react";
-
+import { NextSeo } from "next-seo";
 import "../styles/globals.css";
 import "../styles/heroAnimate.css";
 import "../styles/wave.css";
@@ -9,7 +9,7 @@ import "../styles/journey.css";
 
 import "aos/dist/aos.css";
 import "slick-carousel/slick/slick.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 import Header from "../src/components/Header";
 import AOS from "aos";
@@ -57,6 +57,112 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <div className="font-rubik">
+      <NextSeo
+        title="Hack a chain"
+        description="We are Blockchain Specialists. More than a software service. All our business and technology knowledge working for you."
+        additionalLinkTags={[
+          {
+            rel: "apple-touch-icon",
+            href: "/favicon/apple-icon-57x57.png",
+            sizes: "57x57",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/favicon/apple-icon-60x60.png",
+            sizes: "60x60",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/favicon/apple-icon-72x72.png",
+            sizes: "72x72",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/favicon/apple-icon-76x76.png",
+            sizes: "76x76",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/favicon/apple-icon-114x114.png",
+            sizes: "114x114",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/favicon/apple-icon-120x120.png",
+            sizes: "120x120",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/favicon/apple-icon-144x144.png",
+            sizes: "144x144",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/favicon/apple-icon-152x152.png",
+            sizes: "152x152",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/favicon/apple-icon-180x180.png",
+            sizes: "180x180",
+          },
+
+          {
+            rel: "icon",
+            type: "image/png",
+            href: "/favicon/android-icon-192x192.png",
+            sizes: "192x192",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            href: "/favicon/android-icon-192x192.png",
+            sizes: "192x192",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            href: "/favicon/android-icon-32x32.png",
+            sizes: "32x32",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            href: "/favicon/android-icon-96x96.png",
+            sizes: "96x96",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            href: "/favicon/android-icon-16x16.png",
+            sizes: "16x16",
+          },
+          {
+            rel: "manifest",
+            href: "/favicon/manifest.json",
+          },
+          {
+            rel: "icon",
+            type: "image/png",
+            href: "/favicon/android-icon-16x16.png",
+            sizes: "16x16",
+          },
+        ]}
+        additionalMetaTags={[
+          {
+            name: "msapplication-TileColor",
+            content: "#ffffff",
+          },
+          {
+            name: "msapplication-TileImage",
+            content: "/favicon/ms-icon-144x144.png",
+          },
+          {
+            name: "theme-color",
+            content: "#ffffff",
+          },
+        ]}
+      />
       <Header move={moveHeader} />
       <div id="progressbar" ref={progressBar}></div>
       <div id="scrollPath"></div>
--- src/components/JourneyMobile.jsx
+++ src/components/JourneyMobile.jsx
@@ -1,6 +1,10 @@
 export default function JourneyMobile() {
   return (
-    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+      className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
+    >
       <div className="grid gap-6 row-gap-10 lg:grid-cols-2">
         <div className="lg:py-6 lg:pr-16">
           <div className="flex">
